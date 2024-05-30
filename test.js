9fc93ba0a523cb6d287c9886973ecea1607e5494
Tom Hennessy <tomlhennessy@outlook.com>
2024-05-30 20:09:15 +0100

    used bind to ensure the  context refers to the  object

--- test.js
+++ test.js
@@ -1,1 +1,11 @@
-// Your code here
+
+const Employee = require('./employee');
+
+// Create a new Employee
+const john = new Employee('John Wick', 'Dog Lover');
+
+// Call sayName() in a setTimeout after 2 seconds
+setTimeout(john.sayName.bind(john), 2000);
+
+// Call sayOccupation() in a setTimeout after 3 seconds
+setTimeout(john.sayOccupation.bind(john), 3000);
